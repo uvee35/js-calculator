33a42344c481f7bc4ddbd7ed6f7e136aea67daa6
Victor <victormarquesrina@hotmail.com>
2024-01-20 15:37:15 +0000

    Logic added and bugs fixed

--- script.js
+++ script.js
@@ -2,14 +2,27 @@
 
 const display = document.getElementById("display");
 
-function appendToDisplay (input){
-
+//Display the value
+function appendToDisplay(input){
+display.value += input;
 }
 
+//Clear the value
 function clearDisplay(){
-
+display.value= ""
 }
 
+//Calculate the values
 function calculate(){
+    try{
+        display.value= eval(display.value);
+    }
+    //Display error in case that and operation is incomplete
+    catch(error){
+        display.value = "Error"
+    }
 
 }
+
+
+
